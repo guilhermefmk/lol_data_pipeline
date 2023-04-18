1b4f3267b3d4eca678bb25bd02b0b9b8f15aa1f0
Guilherme <guilhermefmk9@gmail.com>
2023-04-18 17:54:04 -0300

    match_data to gcs done

--- etl_gcs_to_bigquery.py
+++ etl_gcs_to_bigquery.py
@@ -7,6 +7,7 @@ import datetime
 from prefect.blocks.system import Secret
 import time
 import requests
+import os
 
 @task(log_prints=True)
 def get_headers() -> dict:
@@ -44,6 +45,12 @@ def fetch(data: Path) -> pd.DataFrame:
     return df
 
 @task()
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.astype(str)
+    return df
+
+
+@task()
 def get_match_data(match_id: str, header: dict):
     url = f'https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}'
     response = requests.get(url, headers=header)
@@ -55,18 +62,34 @@ def struct_dict(dados) -> list:
     game_id = dados['info']['gameId']
     game_mode = dados['info']['gameMode']
     game_type = dados['info']['gameType']
+    patch = dados['info']['gameVersion']
     participantes = dados['info']['participants']
+    game_creation = dados['info']['gameCreation']
+    game_start = dados['info']['gameStartTimestamp']
+    game_end = dados['info']['gameEndTimestamp']
+    game_duration = dados['info']['gameDuration']
 
     lista_participantes = []
     for p in participantes:
-        d = {'gameId': game_id, 'gameMode': game_mode, 'gameType': game_type}
+        d = {
+                'gameId': game_id, 
+                'gameMode': game_mode, 
+                'gameType': game_type, 
+                'patch': patch,
+                'gameStartTimestamp': game_start,
+                'gameEndTimestamp': game_end,
+                'gameDuration': game_duration,
+                'gameCreation': game_creation
+            }
+        p.pop('challenges', None)
+        p.pop('perks', None)
         d.update(p)
         lista_participantes.append(d)
     return lista_participantes
 
 @task()
 def get_main_df() -> pd.DataFrame:
-    df = pd.DataFrame(columns=['gameId', 'gameMode', 'gameType', 'allInPings', 'assistMePings', 'assists', 'baitPings', 'baronKills', 'basicPings', 'bountyLevel', 'challenges', 'champExperience', 'champLevel', 'championId', 'championName', 'championTransform', 'commandPings', 'consumablesPurchased', 'damageDealtToBuildings', 'damageDealtToObjectives', 'damageDealtToTurrets', 'damageSelfMitigated', 'dangerPings', 'deaths', 'detectorWardsPlaced', 'doubleKills', 'dragonKills', 'eligibleForProgression', 'enemyMissingPings', 'enemyVisionPings', 'firstBloodAssist', 'firstBloodKill', 'firstTowerAssist', 'firstTowerKill', 'gameEndedInEarlySurrender', 'gameEndedInSurrender', 'getBackPings', 'goldEarned', 'goldSpent', 'holdPings', 'individualPosition', 'inhibitorKills', 'inhibitorTakedowns', 'inhibitorsLost', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'itemsPurchased', 'killingSprees', 'kills', 'lane', 'largestCriticalStrike', 'largestKillingSpree', 'largestMultiKill', 'longestTimeSpentLiving', 'magicDamageDealt', 'magicDamageDealtToChampions', 'magicDamageTaken', 'needVisionPings', 'neutralMinionsKilled', 'nexusKills', 'nexusLost', 'nexusTakedowns', 'objectivesStolen', 'objectivesStolenAssists', 'onMyWayPings', 'participantId', 'pentaKills', 'perks', 'physicalDamageDealt', 'physicalDamageDealtToChampions', 'physicalDamageTaken', 'profileIcon', 'pushPings', 'puuid', 'quadraKills', 'riotIdName', 'riotIdTagline', 'role', 'sightWardsBoughtInGame', 'spell1Casts', 'spell2Casts', 'spell3Casts', 'spell4Casts', 'summoner1Casts', 'summoner1Id', 'summoner2Casts', 'summoner2Id', 'summonerId', 'summonerLevel', 'summonerName', 'teamEarlySurrendered', 'teamId', 'teamPosition', 'timeCCingOthers', 'timePlayed', 'totalDamageDealt', 'totalDamageDealtToChampions', 'totalDamageShieldedOnTeammates', 'totalDamageTaken', 'totalHeal', 'totalHealsOnTeammates', 'totalMinionsKilled', 'totalTimeCCDealt', 'totalTimeSpentDead', 'totalUnitsHealed', 'tripleKills', 'trueDamageDealt', 'trueDamageDealtToChampions', 'trueDamageTaken', 'turretKills', 'turretTakedowns', 'turretsLost', 'unrealKills', 'visionClearedPings', 'visionScore', 'visionWardsBoughtInGame', 'wardsKilled', 'wardsPlaced', 'win', 'timestamp'])
+    df = pd.DataFrame(columns=['gameId', 'gameMode', 'gameType', 'patch', 'gameStartTimestamp', 'gameEndTimestamp', 'gameDuration', 'gameCreation','allInPings', 'assistMePings', 'assists', 'baitPings', 'baronKills', 'basicPings', 'bountyLevel', 'champExperience', 'champLevel', 'championId', 'championName', 'championTransform', 'commandPings', 'consumablesPurchased', 'damageDealtToBuildings', 'damageDealtToObjectives', 'damageDealtToTurrets', 'damageSelfMitigated', 'dangerPings', 'deaths', 'detectorWardsPlaced', 'doubleKills', 'dragonKills', 'eligibleForProgression', 'enemyMissingPings', 'enemyVisionPings', 'firstBloodAssist', 'firstBloodKill', 'firstTowerAssist', 'firstTowerKill', 'gameEndedInEarlySurrender', 'gameEndedInSurrender', 'getBackPings', 'goldEarned', 'goldSpent', 'holdPings', 'individualPosition', 'inhibitorKills', 'inhibitorTakedowns', 'inhibitorsLost', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'itemsPurchased', 'killingSprees', 'kills', 'lane', 'largestCriticalStrike', 'largestKillingSpree', 'largestMultiKill', 'longestTimeSpentLiving', 'magicDamageDealt', 'magicDamageDealtToChampions', 'magicDamageTaken', 'needVisionPings', 'neutralMinionsKilled', 'nexusKills', 'nexusLost', 'nexusTakedowns', 'objectivesStolen', 'objectivesStolenAssists', 'onMyWayPings', 'participantId', 'pentaKills', 'physicalDamageDealt', 'physicalDamageDealtToChampions', 'physicalDamageTaken', 'profileIcon', 'pushPings', 'puuid', 'quadraKills', 'riotIdName', 'riotIdTagline', 'role', 'sightWardsBoughtInGame', 'spell1Casts', 'spell2Casts', 'spell3Casts', 'spell4Casts', 'summoner1Casts', 'summoner1Id', 'summoner2Casts', 'summoner2Id', 'summonerId', 'summonerLevel', 'summonerName', 'teamEarlySurrendered', 'teamId', 'teamPosition', 'timeCCingOthers', 'timePlayed', 'totalDamageDealt', 'totalDamageDealtToChampions', 'totalDamageShieldedOnTeammates', 'totalDamageTaken', 'totalHeal', 'totalHealsOnTeammates', 'totalMinionsKilled', 'totalTimeCCDealt', 'totalTimeSpentDead', 'totalUnitsHealed', 'tripleKills', 'trueDamageDealt', 'trueDamageDealtToChampions', 'trueDamageTaken', 'turretKills', 'turretTakedowns', 'turretsLost', 'unrealKills', 'visionClearedPings', 'visionScore', 'visionWardsBoughtInGame', 'wardsKilled', 'wardsPlaced', 'win', 'timestamp'])
     return df
 
 @task()
@@ -74,22 +97,30 @@ def write_bq(df: pd.DataFrame) -> None:
     '''Write DataFrame to BigQuery'''
     gcp_block = GcpCredentials.load("lol-credentials")
     df.to_gbq(
-        destination_table= "lol.first",
+        destination_table= "lol.match_data_raw",
         project_id= "lol-data-project-383712",
         credentials= gcp_block.get_credentials_from_service_account(),
         chunksize= 500_000,
         if_exists="append"
     )
 
+@task()
+def write_gcs(path: str):
+    gcp_block = GcsBucket.load("lol-datalake")
+    gcp_block.upload_from_path(
+        from_path=f'{path}',
+        to_path=path
+    )
+
 
 
 @flow(log_prints=True)
-def etl_gcs_to_bq(df) -> pd.DataFrame:
+def etl_data_match_to_gcs(df) -> pd.DataFrame:
     '''Main ETL flow to load data into Big Query(datawarehouse)'''
     write_bq(df)
     return df
 
-@flow()
+@flow(log_prints=True)
 def etl_sub_flow():
     header = get_headers()
     path = extract_from_gcs()
@@ -99,9 +130,12 @@ def etl_sub_flow():
         time.sleep(1)
         json_response = get_match_data(row['match_id'], header)
         dados_participantes = struct_dict(json_response)
-        df = df_main._append(dados_participantes, ignore_index=True)
+        df_partida = pd.DataFrame(dados_participantes)
+        df_main = pd.concat([df_main, df_partida], ignore_index=True)
     df_main['timestamp'] = pd.Timestamp.now().strftime('%Y-%m-%d')
-    etl_gcs_to_bq(df_main)
+    df_main = transform(df_main)
+  
+    etl_data_match_to_gcs(df_main)
 
 
 if __name__ == "__main__":
